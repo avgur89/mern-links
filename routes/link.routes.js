50a91d27bf0589c24ab09bfc468a6545b79ba81d
oleksandr.gurov <oleksandr.gurov@vaimo.com>
2020-05-02 18:36:51 +0300

    Finished link routes

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const shortid = require('shortid');
 const Link = require('../models/Link');
 const auth = require('../middleware/auth.middleware');
 
 // @desc    Create new link
 // @route   POST /api/link/generate
 // @access  Private
-router.post('/generate', async (req, res) => {
+router.post('/generate', auth, async (req, res) => {
   try {
+    const baseUrl = process.env.BASE_URL;
+    const { from } = req.body;
+
+    const code = shortid.generate();
+    const existing = await Link.findOne({ from });
+
+    if (existing) {
+      return res.status(200).json({ link: existing });
+    }
+
+    const to = baseUrl + '/t/' + code;
+
+    const link = new Link({ code, to, from, owner: req.user.userId });
+
+    await link.save();
+    res.status(201).json({ link });
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
   }
@@ -29,7 +46,7 @@ router.get('/', auth, async (req, res) => {
 // @desc    Get single link
 // @route   GET /api/link/id
 // @access  Private
-router.get('/:id ', async (req, res) => {
+router.get('/:id ', auth, async (req, res) => {
   try {
     const link = await Link.findById(req.params.id);
 
